4ff52ca4072689d26cb9ceb08a0695fc8a5c554d
nomuratouwa <81787326+nomuratouwa@users.noreply.github.com>
2021-04-27 16:27:53 +0900

    リ度み追記、主人公ジャンプ追加

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,47 +4,83 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float Speed;
-    public float Pyon;
-    public int PyonKankaku;
-    private Rigidbody2D rb;
-    private float Cnt1;
-    private float PyonRimit = 1;
+    public float Speed;     //はやさ
+    public float Jump;      //ジャンプの高さ
+    public float Pyon;      //移動するときのぴょんするときの高さ
+    public int PyonKankaku; //ぴょんする間隔
+
+    private Rigidbody2D RB;
+    private float Cnt1;     //ぴょん間隔の計算
+    private float PyonRimit = 1;//↑と同じ
+    private bool Ground = true;  //今地面足ついてる？true そう false ちがいます
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        RB = GetComponent<Rigidbody2D>();
     }
 
-    // 物理演算をしたい場合はFixedUpdateを使うのが一般的
     void FixedUpdate()
     {
-        float horizontalKey = Input.GetAxis("Horizontal");
+        float HorizontalKey = Input.GetAxis("Horizontal");
         float VerticalKey = Input.GetAxis("Vertical");
-        if (Cnt1 < PyonRimit)
+        if (Cnt1 < PyonRimit && HorizontalKey!=0)
         {
-            //右入力で右向きに動く
-            if (horizontalKey > 0)
-            {
-                rb.AddForce(transform.right * Speed);
-                rb.AddForce(transform.up * Pyon);
-                Cnt1 += PyonKankaku;
-            }
-            //左入力で左向きに動く
-            else if (horizontalKey < 0)
-            {
-                rb.AddForce(-transform.right * Speed);
-                rb.AddForce(transform.up * Pyon);
-                Cnt1 += PyonKankaku;
-            }
+            Move(HorizontalKey);
         }
-        //ボタンを話すと止まる
+        //何も押さないとｘ座標だけ止まる
         else
         {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-            if (Cnt1 >= PyonRimit)
-                Cnt1--;
+            RB.velocity = new Vector2(0, RB.velocity.y);
+            if (Cnt1 >= PyonRimit) Cnt1--;
         }
-    
+        if (Ground　&& VerticalKey > 0)
+        {
+            RB.AddForce(transform.up * Jump);
+            Ground = false;
+        }
+    }
+    void Move(float Key)
+    {
+        //右入力で右向きに動く
+        if (Key > 0)
+        {
+            RB.AddForce(transform.right * Speed);
+            Cnt1 += PyonKankaku;
+        }
+        //左入力で左向きに動く
+        else if (Key < 0)
+        {
+            RB.AddForce(-transform.right * Speed);
+            Cnt1 += PyonKankaku;
+        }
+        if (Ground)
+        {
+            RB.AddForce(transform.up * Pyon);
+        }
+    }
+    //着地判定
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            if (!Ground)
+                Ground = true;
+        }
+    }
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            if (!Ground)
+                Ground = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            if (Ground)
+                Ground = false;
+        }
     }
 }
