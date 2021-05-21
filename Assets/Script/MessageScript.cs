c71cb6e63a77c4cfc24ef952918c7f5870625a19
nomuratouwa <81787326+nomuratouwa@users.noreply.github.com>
2021-05-21 10:30:42 +0900

    no message

--- Assets/Script/MessageScript.cs
+++ Assets/Script/MessageScript.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class MessageScript : MonoBehaviour
 {
+    public CanvasGroup canvasGroup;
+    public float FeedTime = 0.01f;
+
+    private bool PlayerHantei = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvasGroup.alpha = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerHantei)
+        {
+            if (canvasGroup.alpha < 1.0f)
+                canvasGroup.alpha += FeedTime;
+        }
+        else
+        {
+            if (canvasGroup.alpha > 0.0f)
+                canvasGroup.alpha -= FeedTime;
+        }
     }
-
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name == "NejiHazushi")
         {
-            transform.Find("Panel").gameObject.SetActive(true);
+            PlayerHantei = true;
         }
     }
     void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.name == "NejiHazushi")
         {
-            transform.Find("Panel").gameObject.SetActive(false);
+            PlayerHantei = false;
         }
     }
 }
