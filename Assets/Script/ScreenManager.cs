df11e90cb6d3eaf53cf9b2d020ab7bd8ae34f6ed
A19002-PC\A19002 <san-a19002@sist.ac.jp>
2021-05-25 16:29:51 +0900

    no message

---
+++ Assets/Script/ScreenManager.cs
@@ -0,0 +1,37 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenManager : MonoBehaviour
+{
+    public void PushStartButton()
+    {
+        SceneManager.LoadScene("StageSelectScene");    //タイトルシーン → ステージセレクトシーン 遷移
+    }
+
+    public void PushStageSelectButton()
+    {
+        SceneManager.LoadScene("StageScene" + stageNo);    //ステージセレクトシーン → それぞれのステージシーン 遷移
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //オプション画面(未実装)
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SceneManager.LoadScene("Option");
+        }
+        
+    }
+
+}*/
