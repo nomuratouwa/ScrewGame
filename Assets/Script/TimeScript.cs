51c77de614999ef833f8d0fc0bfe8a62c05bf68c
nomuratouwa <81787326+nomuratouwa@users.noreply.github.com>
2021-05-31 15:15:21 +0900

    ゲーム終了キー実装えｓｃキー

--- Assets/Script/TimeScript.cs
+++ Assets/Script/TimeScript.cs
@@ -5,36 +5,54 @@ using UnityEngine.UI;
 
 public class TimeScript : MonoBehaviour
 {
-    public float LimitTime = 5.0f;
+    
+    public  float LimitTime = 5.0f;
+    public static float NokoriTime;
+    public float KikenTime = 20.0f;
 
     //時間を表示する変数
     public Text timeText;
+    private GameObject BGMObject;
     private GameObject PlayerObject;
     private bool Sound = false;
     void Start()
     {
         //プレイヤーオブジェクト取得
         PlayerObject = GameObject.Find("driver");
+        //BGMオブジェクト取得
+        BGMObject = GameObject.Find("BGM");
+        NokoriTime = LimitTime;
     }
     // Update is called once per frame
     void Update()
     {
         //時間をカウントダウンする
-        LimitTime -= Time.deltaTime;
+        NokoriTime -= Time.deltaTime;
 
         //時間を表示する
-        timeText.text = "のこり"+LimitTime.ToString("f1");
+        timeText.text = "のこり"+ NokoriTime.ToString("f2");
 
-        if (LimitTime <= 10&&!Sound)
+        if (NokoriTime <= KikenTime && !Sound)
         {
-            Sound = true;
-            GetComponent<AudioSource>().Play();  // 効果音を鳴らす
+            LimitKiken();
         }
         //制限時間になった
-        if (LimitTime <= 0)
+        if (NokoriTime <= 0)
         {
             PlayerObject.GetComponent<PlayerController>().Die();
             timeText.text = ("のこり0.0");
         }
+    }
+
+    void LimitKiken()//制限時間残り・・・
+    {
+        Sound = true;
+        GetComponent<AudioSource>().Play();  // 効果音を鳴らす
+        BGMObject.GetComponent<AudioSource>().pitch = 1.2f;
+    }
+    
+    public static float GetTime()
+    {
+        return NokoriTime;
     }
 }
