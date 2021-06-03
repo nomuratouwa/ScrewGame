88790b9fd4e3825e177c752630603c28c1873d31
A19014-PC\A19014 <a19014@sangi.jp>
2021-06-03 16:37:16 +0900

    作業途中

--- Assets/Script/StuffedRoll.cs
+++ Assets/Script/StuffedRoll.cs
@@ -5,6 +5,16 @@ using UnityEngine.UI;
 
 public class StuffedRoll : MonoBehaviour
 {
+    //　テキストのスクロールスピード
+    [SerializeField]
+    private float textScrollSpeed = 30;
+    //　テキストの制限位置
+    //[SerializeField]
+    //private float limitPosition = 730f;
+    //　エンドロールが終了したかどうか
+    private bool isStopEndRoll;
+    //　シーン移動用コルーチン
+    private Coroutine endRollCoroutine;
     void Start()
     {
         
@@ -12,6 +22,35 @@ public class StuffedRoll : MonoBehaviour
 
     void Update()
     {
-        
+        //　エンドロールが終了した時
+        if (isStopEndRoll)
+        {
+            endRollCoroutine = StartCoroutine(GoToNextScene());
+        }
+        else
+        {
+            //　エンドロール用テキストがリミットを越えるまで動かす
+            if (transform.position.y <= limitPosition)
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + textScrollSpeed * Time.deltaTime);
+            }
+            else
+            {
+                isStopEndRoll = true;
+            }
+        }
+    }
+
+    IEnumerator GoToNextScene()
+    {
+        //　5秒間待つ
+        yield return new WaitForSeconds(5f);
+
+        if (Input.GetKeyDown("space"))
+        {
+            StopCoroutine(endRollCoroutine);
+        }
+
+        yield return null;
     }
 }
